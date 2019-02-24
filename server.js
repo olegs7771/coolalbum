2ea98337b318503ba127b75d2d3100d8daba3af2
Oleg Smushkevich <olegs777@bezeqint.net>
2019-02-24 22:05:49 +0200

    Server running on port5000 connected to mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const app = express();
+
 //db config
 const db = require("./config/keys").mongoDB;
 //connect to mongoDB
@@ -9,3 +11,6 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("connected to mongoDB"))
   .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port${port}`));
