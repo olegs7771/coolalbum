bc3d73902b4423cad99d1d0e2636958da5249370
oleg <olegs777@bezeqint.net>
2019-02-27 14:20:03 +0200

    heroku-postbuild

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 //routers
 const users = require("./routers/api/users");
 
@@ -25,6 +26,15 @@ mongoose
 
 //Use routes
 app.use("/api/users", users);
+
+// Server Static Assets while in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port${port}`));
