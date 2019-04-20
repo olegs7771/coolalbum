b499ac607b2c29f827d62b406ba6de4b34a61b5d
Oleg Smushkevich <olegs777@bezeqint.net>
2019-04-20 21:57:15 +0300

    ok

--- client/src/components/profile/ProfileItems.js
+++ client/src/components/profile/ProfileItems.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import moment from "moment";
 
 class ProfileItems extends Component {
   render() {
     const { name, email, avatar, date } = this.props;
     return (
       <div className="row">
-        <div className="col-md-6">
+        <div className="col-md-8">
           <ul className="list-group-flush my-4 text-left">
             <li className="list-group-item ">
               <span className="text-info">Name </span> {name}
@@ -14,16 +15,14 @@ class ProfileItems extends Component {
               <span className="text-info">Email</span> {email}
             </li>
             <li className="list-group-item ">
-              <span className="text-info">Registered at </span> {date}
+              <span className="text-info">Registered at </span>
+              {moment(date).format("DD/MM/YYYY")}
+              {date}
             </li>
           </ul>
         </div>
-        <div className="col-md-6">
-          <img
-            src={avatar}
-            alt="avatar"
-            className="my-3 rounded-circle border"
-          />
+        <div className="col-md-4">
+          <img src={avatar} className="my-3 rounded-circle border" />
         </div>
       </div>
     );
