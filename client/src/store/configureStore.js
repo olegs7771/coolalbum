9696fd24a2259d481b3e0f1252e9ba7e0a9e7300
Oleg Smushkevich <olegs777@bezeqint.net>
2019-06-13 11:08:57 +0300

    ok

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,7 +1,9 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = require("./configureStore.prod");
+  console.log("production");
 } else {
   module.exports = require("./configureStore.dev");
 }
+console.log("development");
 
 console.log("process.env.NODE_ENV", process.env.NODE_ENV);
