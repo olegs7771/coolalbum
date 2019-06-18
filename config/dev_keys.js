9689a775e719209049f1837c09a947469ff73793
Oleg Smushkevich <olegs777@bezeqint.net>
2019-06-18 21:12:05 +0300

    ok

--- config/dev_keys.js
+++ config/dev_keys.js
@@ -1,7 +1,7 @@
 let dbURL;
-if (process.env.NODE_ENV === "production") {
+if ((process.env.NODE_ENV = "production")) {
   dbURL =
-    "mongodb://olegs777:olegs777@ds139167.mlab.com:39167/coolalbum_production";
+    "mongodb://olegs7777:olegs7777@ds139167.mlab.com:39167/coolalbum_production";
 } else {
   dbURL = "mongodb://olegs777:olegs777@ds139435.mlab.com:39435/coolalbum";
 }
--- config/prod_keys.js
+++ config/prod_keys.js
@@ -2,3 +2,4 @@ module.exports = {
   mongoDB: process.env.MONGO_URI,
   secredOrKey: process.env.SECRET_KEY
 };
+console.log("process.env.MONGO_URI", process.env.MONGO_URI);
