ba7a6d051cd0863eae58a45747d24400e26b7a22
oleg <olegs777@bezeqint.net>
2019-08-06 10:30:09 +0300

    ok

--- client/src/utils/Socket_io.js
+++ client/src/utils/Socket_io.js
@@ -2,8 +2,8 @@
 import io from "socket.io-client";
 
 const Socket_io = () => {
-  // const socket = io("https://infinite-everglades-47869.herokuapp.com");
-  const socket = io("http://localhost:5000");
+  const socket = io("https://infinite-everglades-47869.herokuapp.com");
+  // const socket = io("http://localhost:5000");
   return socket;
 };
 export default Socket_io;
