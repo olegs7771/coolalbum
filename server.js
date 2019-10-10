e5f83b8c59366ef5720ee0e675492ffd66da2d4c
Oleg Smushkevich <olegs777@bezeqint.net>
2019-10-10 16:21:38 +0300

    ok

--- server.js
+++ server.js
@@ -79,18 +79,18 @@ app.use("/api/posts", posts);
 app.use("/api/chat", chat);
 
 // Server Static Assets while in production
-// if (process.env.NODE_ENV === "production") {
-//   // set static folder
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-// }
-// console.log("process.env.PORT server ", process.env.PORT);
-// console.log("process.env.NODE_ENV ", process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  //   // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+console.log("process.env.PORT server ", process.env.PORT);
+console.log("process.env.NODE_ENV ", process.env.NODE_ENV);
 
 const server = app.listen(port, () =>
-  console.log(`App listening on port ${port}`)
+  console.log(`App is listening on port ${port}`)
 );
 
 // Connect to socket.io
