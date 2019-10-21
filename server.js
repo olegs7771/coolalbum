7849620d1844cfcbc4ed3d011e17ca63871b54d0
Oleg Smushkevich <olegs777@bezeqint.net>
2019-10-21 17:09:58 +0300

    ok

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ if (process.env.NODE_ENV === "production") {
   //   // set static folder
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.setHeader("set-cookie", ["SameSite=Strict"]);
+    res.setHeader("set-cookie", ["SameSite=Strict;SameSite=Strict"]);
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
